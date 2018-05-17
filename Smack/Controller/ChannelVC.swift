68a05de9b376c8f909982eb5095f955fdf1f3a7c
Preeteesh Remalli <preeteeshremalli@Preeteeshs-MacBook-Pro.local>
2018-05-17 16:53:51 +0530

    video2

---
+++ Smack/Controller/ChannelVC.swift
@@ -0,0 +1,21 @@
+//
+//  ChannelVC.swift
+//  Smack
+//
+//  Created by Preeteesh Remalli on 17/05/18.
+//  Copyright © 2018 Preeteesh Remalli. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+     self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+    }
+
+  
+    
+
+}
---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,26 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Preeteesh Remalli on 17/05/18.
+//  Copyright © 2018 Preeteesh Remalli. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    
+    //Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action:#selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        
+    }
+
+
+}
