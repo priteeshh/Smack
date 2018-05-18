b070e7ebcebb267a87441d628babbca2ee2ba301
Preeteesh Remalli <priteeshh@gmail.com>
2018-05-18 19:32:26 +0530

    video 9

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -7,7 +7,17 @@
 //
 
 import Foundation
+
+typealias complitionHandler = (_ Success: Bool) -> ()
+
+// URL Constants
+let BASE_URL = "ttps://smackappdb.herokuapp.com/v1"
+let URL_REGISTER = "\(BASE_URL)account/register"
 //Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let TO_UNWIND_CHANNEL = "toUnwindToChannel"
+//UserDefaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedin"
+let USER_EMAIL = "userEmail"
